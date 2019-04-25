cdfb1cb34a5daf3b9a651ed56a53417b5d70bcff
liubola <lby3523@gmail.com>
2019-04-25 08:19:26 +0800

    add bundle dir

--- sangreal_wind/utils/__init__.py
+++ sangreal_wind/utils/__init__.py
@@ -1,2 +1,2 @@
-from .engines import ENGINE, WIND_DB
-from .datetime_handle import dt_handle
+from sangreal_wind.utils.datetime_handle import dt_handle
+from sangreal_wind.utils.engines import BUNDLE_DIR, ENGINE, WIND_DB
