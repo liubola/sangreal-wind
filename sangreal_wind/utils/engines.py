1188912fb0170464f88553dd4eb405c03b1d345e
liubola <lby3523@gmail.com>
2019-04-26 08:48:38 +0800

    keyerror bundle_dir

--- sangreal_wind/utils/engines.py
+++ sangreal_wind/utils/engines.py
@@ -13,6 +13,8 @@ from sqlalchemy import create_engine
 """""" """""" """""" """""" """""" ""
 CONFIG_FILE_NAME = "wind.yaml"
 HOME_PATH = os.path.expanduser(f'~{os.sep}.sangreal{os.sep}wind')
+if not os.path.exists(HOME_PATH):
+    os.makedirs(HOME_PATH)
 CONFIG_FILE = os.path.join(HOME_PATH, CONFIG_FILE_NAME)
 
 YAML_TYPE = f"""
@@ -47,13 +49,15 @@ The yaml' type is like {YAML_TYPE}")
 
 def get_bundle(config, k):
     bundle_config = config.get(k, None)
-    if bundle_config is None:
-        raise ValueError(f"Please check the {CONFIG_FILE} and add {k}!\
-The yaml' type is like {YAML_TYPE}")
-    bundle_dir = bundle_config['dir']
-    if bundle_dir is None:
-        bundle_dir = f"{os.path.expanduser(f'~{os.sep}.sangreal{os.sep}backtest{os.sep}bundle{os.sep}')}"
-    return bundle_dir
+    if bundle_config is not None:
+        #         raise ValueError(f"Please check the {CONFIG_FILE} and add {k}!\
+        # The yaml' type is like {YAML_TYPE}")
+        bundle_dir = bundle_config['dir']
+        if bundle_dir is None:
+            bundle_dir = f"{os.path.expanduser(f'~{os.sep}.sangreal{os.sep}backtest{os.sep}bundle{os.sep}')}"
+        return bundle_dir
+    else:
+        return
 
 
 # 实例化数据库 ENGINE
@@ -61,7 +65,8 @@ WIND_DB, ENGINE = get_db(config, 'wind.config')
 
 # 回测框架本地文件存放的数据
 BUNDLE_DIR = get_bundle(config, 'bundle.config')
-if not BUNDLE_DIR.endswith(os.sep):
-    raise ValueError(f'bundle_dir的路径必须以{os.sep}结尾')
-if not os.path.exists(BUNDLE_DIR):
-    os.makedirs(BUNDLE_DIR)
+if BUNDLE_DIR is not None:
+    if not BUNDLE_DIR.endswith(os.sep):
+        raise ValueError(f'bundle_dir的路径必须以{os.sep}结尾')
+    if not os.path.exists(BUNDLE_DIR):
+        os.makedirs(BUNDLE_DIR)
